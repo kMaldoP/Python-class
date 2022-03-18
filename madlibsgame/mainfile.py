9f6bec6a4897c08eabb2b2780c29f76510fd4cc6
krysmaldonado <krysm2015@gmail.com>
2022-03-18 00:38:46 -0700

    commit test

---
+++ madlibsgame/mainfile.py
@@ -0,0 +1,13 @@
+
+from sciencefiction import *
+
+print("Welcome to the big game of madlibs")
+name = input("What is your name?    ")
+print(f"Welcome {name}! lets create a story!")
+name2 = input(" Please create another random name: ")
+name3 = input(" And give me one more name:  ")
+
+print("              -----LETS BEGIN-----           ")
+input("Press enter to continue")
+print("Choose a story!")
+choice = input("1)Science fiction \n2)Western \n3)Spanish Romance  ")
---
+++ madlibsgame/my_pkg/Sciencefiction.py
@@ -0,0 +1,11 @@
+def ScienceStory(name1, name2, name3):
+    print("Science Ficrtion it is! lets get some info first...")
+    verb = input("enter a verb")
+    adjective = input("now enter an adjective")
+    noun = input(" Now a noun")
+    object = input("pick an object")
+    print("That should be enough to get started and we can figure out the rest as we go..")
+    print("A long time ago, in the far off galaxy of ___  was the glorious nation of the order of the [noun]. A once prosperous nation that was know for its ability to [adjective] prosperity for every man woman and child. In the days since the great [verb] war of 2376, the world has never been the same….")
+    verb2 = input("enter a verb")
+    adjective2 = input("now enter an adjective")
+    noun2 = input(" Now a noun")
